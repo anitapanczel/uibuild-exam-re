8ade450f8c58c0883927c01f1b0f88c749f2c6e0
anitapanczel <anita@panczel.hu>
2021-12-13 11:25:50 +0100

    done

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ window.addEventListener("load", init);
 
 function init() {
     console.log("hello");
+
+    let form = `
+    <form>
+        <h1>Visszajelzés</h1>
+        <div><input type="text" placeholder="Tárgy"></div>
+        <div><textarea placeholder="Megjegyzés" class="megjegyzes"></textarea></div>
+        <div class="flex">
+            <textarea class="checkbox"></textarea>
+            <div class="feltetelek">Elolvastam és elfogadom az <a href="">Adatkezelési Tájékoztatót</a>.</div>
+        </div>
+        <div class="button"><button>Mentés</button></div>
+    </form>`;
+
+    document.getElementById("root").insertAdjacentHTML("beforeend", form);
 }
